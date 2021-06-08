99f851bfb1399b17cf7f1926cb09cd50b2158b55
Zaid Bin Khalid <ranazaid239@gmail.com>
2021-06-08 13:23:06 +0500

    admin dashboard created

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,6 @@
 const admin = require("../firebase");
+const user = require("../models/user");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, next) => {
   // console.log(req.headers); // token
@@ -13,5 +15,18 @@ exports.authCheck = async (req, res, next) => {
     res.status(401).json({
       err: "Invalid or Expired Token",
     });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+
+  const adminUser = await User.findOne({ email }).exec();
+  if (adminUser.role !== "admin") {
+    res.status(403).json({
+      err: "Admin resource. Access Denied",
+    });
+  } else {
+    next();
   }
 };
