f9166ba694535709a5bb3fbfbd1e586312f54d8c
Zaid Bin Khalid <ranazaid239@gmail.com>
2021-06-08 20:59:56 +0500

    admin dashboard frontend

---
+++ server/controllers/category.js
@@ -0,0 +1,50 @@
+const Category = require("../models/category");
+const slugify = require("slugify");
+const category = require("../models/category");
+
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    // let check = await Category.findOne({slug:name})
+    // console.log(check)
+    // if(check){
+    //     return res.status(400).send("Already exists")
+    // }
+    res.json(await new Category({ name, slug: slugify(name) }).save());
+  } catch (err) {
+    // console.log(err.message);
+    res.status(400).json({ err: err.message });
+  }
+};
+
+exports.list = async (req, res) => {
+  res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
+};
+
+exports.read = async (req, res) => {
+  let category = await Category.findOne({ slug: req.params.slug }).exec();
+  res.json(category);
+};
+
+exports.update = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const updated = await Category.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(400).send("Category update failed");
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+    res.json(deleted);
+  } catch (err) {
+    res.status(400).send("Category Deleted failed");
+  }
+};
