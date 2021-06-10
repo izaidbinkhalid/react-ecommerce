1794635aa5c0ea18a67eda8140e670a9e9cc15c1
Zaid Bin Khalid <ranazaid239@gmail.com>
2021-06-10 12:44:05 +0500

    category option created

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -5,20 +5,22 @@ const category = require("../models/category");
 exports.create = async (req, res) => {
   try {
     const { name } = req.body;
-    // let check = await Category.findOne({slug:name})
+    let check = await Category.findOne({ slug: name });
     // console.log(check)
-    // if(check){
-    //     return res.status(400).send("Already exists")
-    // }
+    if (check) {
+      return res.status(400).json({ err: "Category Already Exists" });
+    }
     res.json(await new Category({ name, slug: slugify(name) }).save());
   } catch (err) {
-    // console.log(err.message);
     res.status(400).json({ err: err.message });
   }
 };
 
 exports.list = async (req, res) => {
-  res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
+  let cat = await Category.find();
+  console.log(cat);
+  res.json(cat);
+  // res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
 };
 
 exports.read = async (req, res) => {
@@ -29,12 +31,18 @@ exports.read = async (req, res) => {
 exports.update = async (req, res) => {
   const { name } = req.body;
   try {
-    const updated = await Category.findOneAndUpdate(
-      { slug: req.params.slug },
-      { name, slug: slugify(name) },
-      { new: true }
-    );
-    res.json(updated);
+    let check = await Category.findOne({ slug: name });
+    // console.log(check)
+    if (check) {
+      return res.status(400).json({ err: "Category Already Exists" });
+    } else {
+      const updated = await Category.findOneAndUpdate(
+        { slug: req.params.slug },
+        { name, slug: slugify(name) },
+        { new: true }
+      );
+      res.json(updated);
+    }
   } catch (err) {
     res.status(400).send("Category update failed");
   }
@@ -43,6 +51,7 @@ exports.update = async (req, res) => {
 exports.remove = async (req, res) => {
   try {
     const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+    console.log("deleted", deleted);
     res.json(deleted);
   } catch (err) {
     res.status(400).send("Category Deleted failed");
